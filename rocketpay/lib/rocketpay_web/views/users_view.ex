d2770ca1e0a1e5cee55d5dff180f0546133cb5c9
Fernando <nandosbx@users.noreply.github.com>
2021-03-04 23:37:03 -0300

    Account balance

--- rocketpay/lib/rocketpay_web/views/users_view.ex
+++ rocketpay/lib/rocketpay_web/views/users_view.ex
@@ -1,13 +1,17 @@
 defmodule RocketpayWeb.UsersView do
+  alias Rocketpay.{Account, User}
 
-  alias Rocketpay.User
-  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname}}) do
+  def render("create.json", %{user: %User{account: %Account{id: account_id, balance: balance}, id: id, name: name, nickname: nickname}}) do
     %{
       message: "User created",
       user: %{
         id: id,
         name: name,
-        nickname: nickname
+        nickname: nickname,
+        account: %{
+          id: account_id,
+          balance: balance
+        }
       }
     }
     end
