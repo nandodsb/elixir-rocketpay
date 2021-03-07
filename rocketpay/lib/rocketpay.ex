3f6eef37d34b1c95c58ab0e8d78894655a8827bc
Fernando <nandosbx@users.noreply.github.com>
2021-03-06 23:24:27 -0300

    Account View

--- rocketpay/lib/rocketpay.ex
+++ rocketpay/lib/rocketpay.ex
@@ -1,5 +1,9 @@
 defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: UserCreate
-  
- defdelegate create_user(params), to: UserCreate, as: :call 
+  alias Rocketpay.Accounts.Deposit
+  alias Rocketpay.Accounts.Withdraw
+
+ defdelegate create_user(params), to: UserCreate, as: :call
+ defdelegate deposit(params), to: Deposit, as: :call
+ defdelegate withdraw(params), to: Withdraw, as: :call
 end
